fdff2c75e3250dd2e8c1b06a6435b50e8b2e515a
acosta <acostapaez913@gmail.com>
2025-07-31 23:34:56 -0500

    first commit

---
+++ src/handlers/product.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import Product from "../models/Product.model";
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({
+      order: [["id", "DESC"]],
+      limit: 2,
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.json({ data: products });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  // Validacion
+  //   await check("name")
+  //     .notEmpty()
+  //     .withMessage("El nombre no puede ir vacio")
+  //     .run(req);
+  //   await check("price")
+  //     .isNumeric()
+  //     .withMessage("Valor no valido")
+  //     .notEmpty()
+  //     .withMessage("El precio no puede ir vacio")
+  //     .custom((value) => value > 0)
+  //     .withMessage("Precio no valido")
+  //     .run(req);
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+  await product.update(req.body);
+  await product.save();
+  res.json({ data: product });
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+  product.availability = !product.dataValues.availability;
+  await product.save();
+  res.json({ data: product });
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  await product.destroy();
+  res.json({ data: "Producto elimindo" });
+};
---
+++ src/router.ts
@@ -0,0 +1,68 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/product";
+import { handleInputErrors } from "./middleware";
+
+const router = Router();
+
+router.get("/", getProducts);
+
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  getProductById
+);
+
+router.post(
+  "/",
+  // Validacion
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .notEmpty()
+    .withMessage("El precio no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no valido"),
+  // Ejecuta Middleware
+  handleInputErrors,
+  createProduct
+);
+
+router.put(
+  "/:id",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .notEmpty()
+    .withMessage("El precio no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no valido"),
+  body("availability").isBoolean().withMessage("Valor no valido"),
+  handleInputErrors,
+  updateProduct
+);
+
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  updateAvailability
+);
+
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  deleteProduct
+);
+export default router;
