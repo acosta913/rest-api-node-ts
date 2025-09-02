36b6816f261f1d1952ab9aef9b7ce1b144445a80
acosta <acostapaez913@gmail.com>
2025-09-01 21:41:33 -0500

    :construction: package build

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(process.env.DB_CONN_STRING!, {
-  models: [__dirname + "/../models/**/*.ts"],
+  models: [__dirname + "/../models/**/*"],
 });
 
 export default db;
